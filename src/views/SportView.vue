e6a644c101f287f80ee0a5919ee4382f6e73c923
Panithan Bunrong <Panithan.bunrong@gmail.com>
2022-05-12 18:14:18 +0700

    Delete MenuSport

--- src/views/SportView.vue
+++ src/views/SportView.vue
@@ -3,7 +3,7 @@
     <ImgAnimation />
     <div class="row mt-5">
       <div class="col-3">
-        <Menusport />
+       
       </div>
       <div class="col-9">
         <Tabelsport />
@@ -15,12 +15,12 @@
 <script>
 import ImgAnimation from "@/components/ImgAnimation.vue";
 import Tabelsport from "@/components/Tabelsport.vue";
-// import Menusport from "@/components/CardAnimation.vue";
+
 export default {
   components: {
     ImgAnimation,
     Tabelsport,
-    // Menusport,
+    
   },
 };
 </script>
